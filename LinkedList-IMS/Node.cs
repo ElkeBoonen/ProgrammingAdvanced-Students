9ed635a6dee60ff92ab08971bd2963cd83717473
Elke Boonen <u0116591@thomasmore.be>
2020-09-30 15:41:31 +0200

    30/9

---
+++ LinkedList-IMS/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_IMS
+{
+    class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
